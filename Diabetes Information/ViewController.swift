375800c6ff1a78970b726f56f419ed5b75723d76
Wesley Cates <wsc0010@sdmac.cse.eng.auburn.edu>
2016-01-31 16:00:47 -0600

    Initial Commit

---
+++ Diabetes Information/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diabetes Information
+//
+//  Created by Wesley Cates on 1/31/16.
+//  Copyright © 2016 Comp 4710 Group 5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
