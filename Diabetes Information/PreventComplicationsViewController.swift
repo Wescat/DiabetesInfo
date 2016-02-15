36f4931bae4880698b680c77747f7c66b2823f90
George Towles <gwt0002@sdmac.cse.eng.auburn.edu>
2016-02-15 14:19:47 -0600

    Transitions implemented for the preventing complications section of the complications module. Menu created for the complications module. Complications module menu properly colored to match the main menu button.

---
+++ Diabetes Information/PreventComplicationsViewController.swift
@@ -0,0 +1,83 @@
+//
+//  GeneralTipsViewController.swift
+//  Diabetes Information
+//
+//  Created by Ennis Smith on 2/4/16.
+//  Copyright © 2016 Comp 4710 Group 5. All rights reserved.
+//
+
+import UIKit
+
+class PreventComplicationsViewController: UIPageViewController{
+    
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newViewController("PreventComplications1"),
+            self.newViewController("PreventComplications2"),
+            self.newViewController("PreventComplications3"),
+            self.newViewController("PreventComplications4")]
+    }()
+    
+    private func newViewController(view: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewControllerWithIdentifier("\(view)")
+    }
+    
+    override func viewDidLoad(){
+        
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                direction: .Forward,
+                animated: true,
+                completion: nil)
+        }
+        
+    }
+}
+
+extension PreventComplicationsViewController: UIPageViewControllerDataSource{
+    
+    
+    func pageViewController(pageViewController: UIPageViewController,
+        viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+            guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+                return nil
+            }
+            
+            let previousIndex = viewControllerIndex - 1
+            
+            guard previousIndex >= 0 else {
+                return nil
+            }
+            
+            guard orderedViewControllers.count > previousIndex else {
+                return nil
+            }
+            
+            return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController,
+        viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+            guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+                return nil
+            }
+            
+            let nextIndex = viewControllerIndex + 1
+            let orderedViewControllersCount = orderedViewControllers.count
+            
+            guard orderedViewControllersCount != nextIndex else {
+                return nil
+            }
+            
+            guard orderedViewControllersCount > nextIndex else {
+                return nil
+            }
+            
+            return orderedViewControllers[nextIndex]
+    }
+}
