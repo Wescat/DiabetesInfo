61602a5c7bb56a0f11d209c1d3ee019c096d9a1b
George Towles <gwt0002@sdmac.cse.eng.auburn.edu>
2016-02-19 14:47:04 -0600

    Added the other complications section and added the color for the complications view controllers.

---
+++ Diabetes Information/SickDaysViewController.swift
@@ -0,0 +1,84 @@
+//
+//  PreventComplicationsViewController.swift
+//  Diabetes Information
+//
+//  Created by George Towles on 2/15/16.
+//  Copyright © 2016 Comp 4710 Group 5. All rights reserved.
+//
+
+import UIKit
+
+class SickDaysViewController: UIPageViewController{
+    
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newViewController("SickDays1"),
+            self.newViewController("SickDays2"),
+            self.newViewController("SickDays3"),
+            self.newViewController("SickDays4"),
+            self.newViewController("SickDays5")]
+    }()
+    
+    private func newViewController(view: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewControllerWithIdentifier("\(view)")
+    }
+    
+    override func viewDidLoad(){
+        
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                direction: .Forward,
+                animated: true,
+                completion: nil)
+        }
+        
+    }
+}
+
+extension SickDaysViewController: UIPageViewControllerDataSource{
+    
+    
+    func pageViewController(pageViewController: UIPageViewController,
+        viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+            guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+                return nil
+            }
+            
+            let previousIndex = viewControllerIndex - 1
+            
+            guard previousIndex >= 0 else {
+                return nil
+            }
+            
+            guard orderedViewControllers.count > previousIndex else {
+                return nil
+            }
+            
+            return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController,
+        viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+            guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+                return nil
+            }
+            
+            let nextIndex = viewControllerIndex + 1
+            let orderedViewControllersCount = orderedViewControllers.count
+            
+            guard orderedViewControllersCount != nextIndex else {
+                return nil
+            }
+            
+            guard orderedViewControllersCount > nextIndex else {
+                return nil
+            }
+            
+            return orderedViewControllers[nextIndex]
+    }
+}
---
+++ SickDaysViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SickDaysViewController.swift
+//  Diabetes Information
+//
+//  Created by George Towles on 2/19/16.
+//  Copyright © 2016 Comp 4710 Group 5. All rights reserved.
+//
+
+import UIKit
+
+class SickDaysViewController: NSObject {
+
+}
