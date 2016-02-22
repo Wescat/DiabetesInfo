80092ca9ed4e1f6b85f99719199b8e4f25d80445
Ennis Smith <ebs0009@sdmac.cse.eng.auburn.edu>
2016-02-22 12:58:53 -0600

    Recommit due to missing file

---
+++ Diabetes Information/ExerciseAndBloodViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ExerciseAndBloodViewController.swift
+//  Diabetes Information
+//
+//  Created by Ennis Smith on 2/22/16.
+//  Copyright © 2016 Comp 4710 Group 5. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseAndBloodViewController: UIPageViewController{
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newViewController("ExerciseandBlood1"),
+            self.newViewController("ExerciseandBlood2"),
+            self.newViewController("ExerciseandBlood3"),
+            self.newViewController("ExerciseandBlood4"),
+            self.newViewController("ExerciseandBlood5"),
+            self.newViewController("ExerciseandBlood6"),
+            self.newViewController("ExerciseandBlood7")]
+    }()
+    
+    
+    private func newViewController(view: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewControllerWithIdentifier("\(view)")
+    }
+    
+    override func viewDidLoad(){
+        
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                direction: .Forward,
+                animated: true,
+                completion: nil)
+        }
+        
+    }
+}
+
+extension ExerciseAndBloodViewController: UIPageViewControllerDataSource{
+    
+    
+    func pageViewController(pageViewController: UIPageViewController,
+        viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+            guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+                return nil
+            }
+            
+            let previousIndex = viewControllerIndex - 1
+            
+            guard previousIndex >= 0 else {
+                return nil
+            }
+            
+            guard orderedViewControllers.count > previousIndex else {
+                return nil
+            }
+            
+            return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController,
+        viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+            guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+                return nil
+            }
+            
+            let nextIndex = viewControllerIndex + 1
+            let orderedViewControllersCount = orderedViewControllers.count
+            
+            guard orderedViewControllersCount != nextIndex else {
+                return nil
+            }
+            
+            guard orderedViewControllersCount > nextIndex else {
+                return nil
+            }
+            
+            return orderedViewControllers[nextIndex]
+    }
+}
